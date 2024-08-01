c7bb3d3e8d5e8c8f3d840cd75ca5da91f895462c
Robert Burston <robbieburston@gmail.com>
2024-08-01 11:21:16 +0200

    Add count code
    added html and js code for count

---
+++ JSTest/Counter/index.js
@@ -0,0 +1,18 @@
+let count = 0;
+let displayCount = document.getElementById("count")
+let countIncrease = documentGetElementById("increase")
+let countDecrease = documentGetElementById("decrease")
+let reset = documentGetElementById("reset")
+
+function Increment () {
+    count++;
+}
+
+function Decrement () {
+    count--;
+}
+
+function Reset () {
+    count = 0;
+}
+
